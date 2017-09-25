a0d55478e2cb4b8c41cd0d3bb22f039f00f29a98
braverokmc79 <braverokmc79@gmail.com>
2017-09-25 15:34:58 +0900

    jsp 게시판 만들기에서 게시글 입력 - 43

---
+++ Board/WebContent/BoardWriterProc.jsp
@@ -0,0 +1,28 @@
+<%@page import="model.BoardDAO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!-- 
+강의와 다르게 html 적 요소는 제거하였다. 왜냐하면은 BoardWriterProc.jsp 페이지에서는
+userBean 작업만 하기 때문이다. -->
+<%
+	request.setCharacterEncoding("UTF-8");//한글지원
+%>
+<!-- 게시글 작성에 데이터를 한번에 읽어드림 -->
+<jsp:useBean id="boardbean" class="model.BoardBean">
+ <jsp:setProperty name="boardbean" property="*" />
+</jsp:useBean>
+
+<%
+	//데이터 베이스 쪽으로 변경클래스를 넘겨줌
+	BoardDAO bdao =new BoardDAO();
+
+	//데이커 저장 메소드를 호출
+	bdao.insertBoard(boardbean);
+
+%>
+
+
+
+
+
+    
--- Board/src/model/BoardDAO.java
+++ Board/src/model/BoardDAO.java
@@ -32,9 +32,81 @@ public class BoardDAO {
 		}
 	}
 	
+	//하나의 새로운 게시글이 넘어와서 저장되는 메소드
+	public void insertBoard(BoardBean bean){
+		//빈클래스에 넘어오지 않았던 데이터들을 초기화 해주어야 합니다.
+		
+		getCon();
+		//빈클래스에 넘어오지 않았던 데이터들을 초기화 해주어야 합니다.
+		int ref=0; //글그룹을 의미 = 쿼리를 실행시켜서 가장큰 ref 값을 자져온 후  +1을 더해주면됨
+		int re_step=1; //새글이기에 = 부모글
+		int re_level=1; 
+		try{
+			
+			
+			//가장 큰 ref값을 읽어오는 쿼리 준비
+			String refsql ="select max(ref) from board ";
+			//쿼리실행 객체 
+			pstmt =con.prepareStatement(refsql);
+			//쿼리실행후 결과를 리턴
+			rs=pstmt.executeQuery();
+			if(rs.next()){ //결과 값이 였다면
+				ref =rs.getInt(1)+1;//최대값에 +1 를 더해서 글그룹을 설정
+			}
+			//실제로 게시글 전체값을 테이블에 저장
+//			String sql =" insert into board values( board_seq.NEXTVAL, ?, ? , ?, ? , )";
+			
+		String sql =" insert into BOARD (NUM, WRITER, EMAIL, SUBJECT, PASSWORD, REG_DATE, REF, ";
+			   sql +="	REF_STEP, REF_LEVEL, READCOUNT, CONTENT) "; 
+			   sql +=" values(board_seq.NEXTVAL, ? ,? , ?, ?, sysdate, ?, ?, ? , 0, ? ) ";
+			pstmt=con.prepareStatement(sql);
+			//?에 값을 맵핑	  	
+			pstmt.setString(1, bean.getWriter());
+			pstmt.setString(2, bean.getEmail());
+			pstmt.setString(3, bean.getSubject());
+			pstmt.setString(4, bean.getPassword());
+			pstmt.setInt(5, ref);
+			pstmt.setInt(6, re_step);
+			pstmt.setInt(7, re_level);
+			pstmt.setString(8, bean.getContent());
+			//쿼리를 실행하시오
+			pstmt.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				//자원 반납
+				if(pstmt!=null)con.close();
+				if(con!=null)con.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
