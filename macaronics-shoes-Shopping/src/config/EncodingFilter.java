1eca733f8b3e4770d00a91357fe9bf59dc46e812
braverokmc79 <braverokmc79@gmail.com>
2017-10-03 00:58:43 +0900

    jsp  logging  설정하기 - 쇼핑몰(shoes ) 프로젝트 - CJH - 3

--- macaronics-shoes-Shopping/src/config/EncodingFilter.java
+++ macaronics-shoes-Shopping/src/config/EncodingFilter.java
@@ -9,25 +9,30 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 // 어노테이션(annotation, 코드에 대한 주석)
 @WebFilter("/*")  // 필터와 url pattern 매핑
 public class EncodingFilter implements Filter {
 	private String charset="utf-8";
+	
+	final Logger logger = LogManager.getLogger(EncodingFilter.class);
 	public void destroy() {
-		//System.out.println("필터가 종료되었습니다.");
+		logger.info("필터가 종료되었습니다.");
 	}
 	//요청이 들어올 때 거쳐가는 코드
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// place your code here
 		//선처리할 코드 
 		request.setCharacterEncoding(charset);
-		//System.out.println("필터가 실행되었습니다.");
+		logger.info("필터가 실행되었습니다.");
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
 	}
 	//필터를 초기화시킴
 	public void init(FilterConfig fConfig) throws ServletException {
-		//System.out.println("필터가 초기화되었습니다.");
+		logger.info("필터가 초기화되었습니다.");
 	} 
 }
 
---
+++ macaronics-shoes-Shopping/src/config/TestLog.java
@@ -0,0 +1,41 @@
+package config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebServlet("/TestLog")
+public class TestLog extends HttpServlet {
+
+	public static Logger logger2 = LogManager.getFormatterLogger(TestLog.class);
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		final Logger logger = LogManager.getLogger(TestLog.class);
+
+		logger.debug("debug, servlet! {}", "debug- aaa");
+		logger.info("Hello,  {}", "info- aaa");
+		logger.error("error, servlet! {}", "error- aaa");
+		logger.warn("warn, servlet! {}", "warn- aaa");
+		logger.fatal("fatal, servlet! {}", "fatal- aaa");
+
+		logger2.debug("Logging in user %s with birthday %s", "macaronics", "20171003");
+		logger2.debug("Integer.MAX_VALUE = %,d", Integer.MAX_VALUE);
+		logger2.debug("Long.MAX_VALUE = %,d", Long.MAX_VALUE);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
